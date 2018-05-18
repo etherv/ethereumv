889009c3fd0e16547da764420b9d67efc23f3e0e
EtherV <ethereumv@protonmail.com>
2018-05-18 12:46:55 +0900

    comment

--- swarm/api/http/roundtripper.go
+++ swarm/api/http/roundtripper.go
@@ -25,12 +25,12 @@ import (
 
 /*
 http roundtripper to register for bzz url scheme
-see https://github.com/ethereum/go-ethereum/issues/2040
+see https://github.com/ethereumv/ethereumv/issues/2040
 Usage:
 
 import (
- "github.com/ethereum/go-ethereum/common/httpclient"
- "github.com/ethereum/go-ethereum/swarm/api/http"
+ "github.com/ethereumv/ethereumv/common/httpclient"
+ "github.com/ethereumv/ethereumv/swarm/api/http"
 )
 client := httpclient.New()
 // for (private) swarm proxy running locally
